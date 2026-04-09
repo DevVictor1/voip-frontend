4a8dfdff956fef5aee90b10732c75a1099d8731b
Devvictor <appvictorr@email.com>
2026-04-09 03:37:47 +0100

    fix: remove invisible characters from import statement in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-﻿import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import './App.css';
 import MainLayout from './layout/MainLayout';
