72f8f6b580115c5ad569b70d825cfbae5f2184d8
Devvictor <appvictorr@email.com>
2026-04-16 15:16:33 +0100

    feat: update BASE_URL to point to the new backend endpoint

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-﻿const BASE_URL = "https://voip-system-backend.onrender.com";
+﻿const BASE_URL = "https://kaylad-voip-backend-c4bufyetadgjb9ex.canadacentral-01.azurewebsites.net";
 
 export default BASE_URL;
